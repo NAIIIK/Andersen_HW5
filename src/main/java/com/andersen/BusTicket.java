1d1b72df56da3d2abf0c7c8cc75284b6e6af6f88
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-18 22:16:00 +0200

    JFB-5: add BusTicket class

---
+++ src/main/java/com/andersen/BusTicket.java
@@ -0,0 +1,53 @@
+package com.andersen;
+
+public class BusTicket {
+    private String ticketClass;
+
+    private String ticketType;
+
+    private String startDate;
+
+    private String price;
+
+    public String getTicketClass() {
+        return ticketClass;
+    }
+
+    public void setTicketClass(String ticketClass) {
+        this.ticketClass = ticketClass;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "BusTicket{" +
+                "ticketClass='" + ticketClass + '\'' +
+                ", ticketType='" + ticketType + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
